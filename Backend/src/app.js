fa7c01d1e5c98bb49b3369f8c9cce14ed6e631ca
Ravi Kasodariya <ravikasodariya7979@gmail.com>
2026-03-19 23:36:41 +0000

    implement Frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,22 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import authRouter from './routes/auth.routes.js';
+import morgan from "morgan";
+import cors from "cors";
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true,
+  methods:["GET", "POST", "DELETE", "PUT"]
+}))
+
 
 app.get('/', (req, res) => {
   res.json({message: 'Hello, World!'});
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -5,8 +5,3 @@ const model = new ChatGoogleGenerativeAI({
   apiKey: process.env.GEMINI_API_KEY
 });
 
-export async function testAi() {
-    model.invoke("how to get WEB DEVELOPER job in uk in current market of 2026").then((response) => {
-        console.log(response.text);
-    })
-}
